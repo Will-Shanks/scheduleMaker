d81668f6ca2f469e8263e221d2dfbfbd1fd92d6f
Will Shanks <92five@gmail.com>
2017-12-02 17:51:45 -0700

    WIP something wrong with getCliques

--- src/Scheduler.py
+++ src/Scheduler.py
@@ -1,10 +1,12 @@
 import MySQLdb
 from collections import defaultdict
 import itertools
+import copy
 import secrets
 
 db = MySQLdb.connect(host=secrets.DBHOST, user=secrets.DBUSER, passwd=secrets.DBPASSWD, db=secrets.DBNAME)
 cur = db.cursor()
+
 
 class Node:
     def __init__(self, sections):
@@ -43,108 +45,83 @@ class Node:
                         i += 1
         return True
 
+# Takes course/section ID
+# Returns matrix of Nodes (course component x section) (each section is a node)
+def getCourseComponents(course):
+    courseComponents = []
+    sectionCode = False
+    sectionComp = False
+    courseCode = course
+    if '-' in course:
+        courseCode = course.split('-')
+        sectionCode = courseCode[1]
+        courseCode = courseCode[0]
 
-def getSections(courses):
-    def getCourse(course):
-        sectionCode = False
-        sectionComp = False
-        courseCode = course
-        if '-' in course:
-            courseCode = course.split('-')
-            sectionCode = courseCode[1]
-            courseCode = courseCode[0]
+    cur.execute("SELECT id FROM course WHERE code='" + courseCode + "';")
+    courseID = str(cur.fetchone()[0])
+    if sectionCode:
+        cur.execute(
+            "SELECT component FROM SECTION WHERE section = '" + sectionCode + "' and  course_id='" + courseID + "';")
+        sectionComp = cur.fetchone()[0]
 
-        cur.execute("SELECT id FROM course WHERE code='" + courseCode + "';")
-        courseID = str(cur.fetchone()[0])
-        if sectionCode:
+    cur.execute(("SELECT Distinct component FROM section WHERE course_id='" + courseID + "';"))
+    sectionID = cur.fetchall()
+    # add each component of each class to sections as its own list
+    for sect in sectionID:
+        if sectionComp == sect[0]:
             cur.execute(
-                "SELECT component FROM SECTION WHERE section = '" + sectionCode + "' and  course_id='" + courseID + "';")
-            sectionComp = cur.fetchone()[0]
+                "SELECT id FROM section WHERE course_id='" + courseID + "' AND section ='" + sectionCode + "';")
+        else:
+            cur.execute(
+                "SELECT id FROM section WHERE course_id='" + courseID + "' AND component ='" + sect[0] + "';")
+        componentSectionIDs = cur.fetchall()
+        component = []
+        for compSectID in componentSectionIDs:
+            component.append(Node(compSectID))
+        courseComponents.append(component)
+    return courseComponents
 
-        cur.execute(("SELECT Distinct component FROM section WHERE course_id='" + courseID + "';"))
-        sectionID = cur.fetchall()
-        # add each component of each class to sections as its own list
-        for sect in sectionID:
-            if sectionComp == sect[0]:
-                cur.execute(
-                    "SELECT * FROM section WHERE course_id='" + courseID + "' AND section ='" + sectionCode + "';")
-            else:
-                cur.execute(
-                    "SELECT * FROM section WHERE course_id='" + courseID + "' AND component ='" + sect[0] + "';")
-            sections.append(cur.fetchall())
-    sections = []
-    if isinstance(courses, str):
-        getCourse(courses)
-    else:
-        for course in courses:
-            getCourse(course)
-    sections.sort(key=lambda course: len(
-        course))  # sorts sections so the courses with least sections are first for performance
-    return sections
+# Takes matrix of Nodes
+# Returns dict of lists node: [edges]
+def buildGraph(vertexNodes):
+    graph = defaultdict(set)
+    for i in range(len(vertexNodes) - 1):
+        for j in range(len(vertexNodes[i])):
+            k = len(vertexNodes) - 1
+            while k > i:
+                for l in range(len(vertexNodes[k])):
+                    if vertexNodes[i][j].isCompat(vertexNodes[k][l]):
+                        graph[vertexNodes[i][j]].add(vertexNodes[k][l])
+                        graph[vertexNodes[k][l]].add(vertexNodes[i][j])
+                k -= 1
+    return graph
 
-def buildChoiceGraph(sections):
-    def isCompat(a, b):
-        # index 6 is string of days section meets ex: MTWRF
-        # index 4 is start time of section meeting (24hr)
-        # index 5 is finish time of section meeting (24hr)
-        if (a[6] == "ONLINE" or b[6] == "ONLINE"):  # online classes don't have time conflicts
-            return True
-        for day in a[6]:
-            if day in b[6]:
-                if (a[4] < b[4]):  # if a starts before b starts
-                    if (a[5] > b[4]):  # a must finish before b starts
-                        return False
-                elif (a[4] < b[5]):  # b must finish before a starts
-                    return False
-                break  # if compatible on 1 day where both meet, always compatible
-        return True
-
-    intersect = defaultdict(set)
-    for i in range(len(sections)):  # traverse through each course
-        for j in range(len(sections[i])):  # traverse through each section of a course
-
-            sectID = str(sections[i][j][0])
-            for k in range(1, len(
-                    sections) - i):  # traverse relevant courses (row i + 1 is first row below self in matrix)
-                for l in range(len(sections[k + i])):  # traverse through sections of each course
-                    if isCompat(sections[i][j], sections[i + k][l]):
-                        newSectID = str(sections[i + k][l][0])
-                        intersect[sectID].add(newSectID)
-                        intersect[newSectID].add(sectID)
-    return intersect
-
-def getCliques(edges, minCourses, numSections):
+# Takes dict of lists of nodes and their edges
+# Returns list of nodes where each node is a clique of size <numNodesToChoose> of the input nodes
+def getCliques(graphEdges, numNodesToChoose):
     def addComplete():
-        nonlocal solutions
-        if isinstance(compsub[0], Node):
-            if len(compsub) < minCourses:
-                return
-            solutions.add(tuple(compsub))
+        if len(compsub) < numNodesToChoose:
             return
+
         courses = defaultdict(list)
-        for section in compsub:
-            cur.execute("SELECT course_id FROM SECTION WHERE id = '" + section + "';")
-            courseCode = cur.fetchone()[0]
-            courses[str(courseCode)].append(section)
-        for course in list(courses.keys()):
+        for item in compsub:
+            for sectID in item.sections:
+                cur.execute("SELECT course_id FROM SECTION WHERE id = '" + str(sectID) + "';")
+                courseCode = cur.fetchone()[0]
+                courses[str(courseCode)].append(sectID)
+        for course in courses.keys():
             cur.execute("SELECT COUNT(DISTINCT component) FROM section WHERE course_id =" + course + ";")
             if (len(courses[course]) != cur.fetchone()[0]):
                 courses.pop(course)
-        if minCourses == -1:
-            if len(compsub) < (numSections - 1):
-                return
-        if len(courses) < minCourses:
+        if len(courses) < numNodesToChoose:
             return
-
-        courseCombos = list(itertools.combinations(courses, minCourses))
+        courseCombos = list(itertools.combinations(courses, numNodesToChoose))
+        nonlocal solutions
         for combo in courseCombos:
             option = []
             for thing in combo:
-                option.append(courses[thing])
-            option = [item for sublist in option for item in sublist]
-            option.sort(key=lambda x: int(x))
-            option = tuple(option)
-            solutions.add(option)
+                option += courses[thing]
+            solutions.append(Node(option))
 
     def recurfunct(candidates, nays):
         nonlocal compsub
@@ -158,94 +135,59 @@ def getCliques(edges, minCourses, numSections):
                 compsub.append(selected)
                 recurfunct(candidatesTemp, naysTemp)
                 compsub.pop()
-                nays.add(selected)
+                nays.append(selected)
 
     def getConnected(vertex, oldSet):
-        newSet = set()
-        if isinstance(vertex, Node):
-            for neighbor in edges[vertex]:
+        newSet = []
+        for neighbor in graphEdges[vertex]:
+            try:
                 if neighbor in oldSet:
-                    newSet.add(neighbor)
-        else:
-            for neighbor in edges[str(vertex)]:
-                if neighbor in oldSet:
-                    newSet.add(neighbor)
+                    newSet.append(neighbor)
+            except Exception as inst:
+                pass
+        if newSet is None:
+            pass
         return newSet
 
     compsub = []
-    solutions = set()
-    possibles = set(edges.keys())
-    recurfunct(possibles, set())
+    solutions = []
+    possibles = list(graphEdges.keys())
+    recurfunct(possibles, [])
     return solutions
 
-def getChoiceOptions(choice):
-    sections = getSections(choice[0])
-    if len(sections) == 1:
-        return set([sections[0][0][0]])
-    edges = buildChoiceGraph(sections)
-    options = getCliques(edges, choice[1], len(sections))
-    return options
-
-def buildSchedGraph(choiceCliques):
-    vertexNodes = []
-    for choiceList in choiceCliques:
-        choiceNodes = []
-        for choice in choiceList:
-            choiceNodes.append(Node(choice))
-        vertexNodes.append(choiceNodes)
-    graph = defaultdict(set)
-    for i in range(len(vertexNodes) - 1):
-        for j in range(len(vertexNodes[i])):
-            k = len(vertexNodes) - 1
-            while k > i:
-                for l in range(len(vertexNodes[k])):
-                    if vertexNodes[i][j].isCompat(vertexNodes[k][l]):
-                        graph[vertexNodes[i][j]].add(vertexNodes[k][l])
-                        graph[vertexNodes[k][l]].add(vertexNodes[i][j])
-                k -= 1
-    return graph
-
-def getScheds(choiceCliques, minChoices, hasRequired):
-    graph = buildSchedGraph(choiceCliques)
-    scheds = getCliques(graph, minChoices, len(choiceCliques))
-    return scheds
-
-def printSched(scheds):
-    for sectionID in scheds:
-        cur.execute("SELECT course_id, component, section FROM SECTION WHERE id = '" + str(sectionID) + "';")
-        sectionInfo = cur.fetchone()
-        cur.execute("SELECT name, code FROM course WHERE id='" + str(sectionInfo[0]) + "';")
-        courseInfo = cur.fetchone()
-        print(courseInfo[1] + "-" + str(sectionInfo[2]) + "     " + courseInfo[0] + "  " + sectionInfo[1])
-    print("============================================")
-
-
-def main():
-    choices = [[["Math3510", "CSCI3104"], 2], [["HIND1020"], 1]]#, [["CSCI3155"], 1], [["CSCI3022"], 1], [["PHYS1140"], 1]]
-    # a choice is given as: [[<courses>], <number to choose>]
-    # if choice[1] is not an int, and is == -1 then this is the list of required courses/sections
-    choiceOptions = []
+def main(choices):
+    # options will be matrix of section nodes
+    options = []
+    # choice == ["course1", ... "coursen"], numToChoose
+    # makes matrix of nodes where each node is a clique
     for choice in choices:
-        choiceOptions.append(getChoiceOptions(choice))
-    scheds = getScheds(choiceOptions, len(choices), -1 == choices[0][1]) #if choices[0][1] == -1 then there is a list of required
+        # getSections will return list of lists of nodes sections[course component][section]
+        if isinstance(choice[0], str):
+            components = getCourseComponents(choice[0])
+        else:
+            components = []
+            for courseID in choice[0]:
+                components += getCourseComponents(courseID)
 
-    sorted = []
-    for sched in scheds:
-        temp = []
-        for choice in sched:
-            if isinstance(choice.sections, int):
-                temp.append(choice.sections)
-            else:
-                for sect in choice.sections:
-                    temp.append(int(sect))
-        temp.sort()
-        sorted.append(temp)
-    sorted.sort()
-    for sched in sorted:
-        printSched(sched)
-    #print("bye!")
-    print(len(scheds))
+        if len(components) == 1:
+            # if only 1 section then only 1 node in graph and no edges
+            options.append([components[0][0]])
+        else:
+            components.sort(key=lambda comp: len(comp))
+            # edges is a list of edges for each node
+            # edges[i] = [[edges], [sectionIDs]]
+            edges = buildGraph(components)
+            # getCliques returns list of nodes (each possible clique is a node
+            #getCliques(list of edges, num of nodes to choose
+            options.append(getCliques(edges, choice[1]))
 
-if __name__== "__main__":
-    main()
+    options.sort(key=lambda comp: len(comp))
+    edges = buildGraph(options)
+    options = getCliques(edges, len(choices))
+    pass
+
+
+if __name__ == "__main__":
+    choices = [[["Math3510", "CSCI3104"], 2], [["HIND1020"], 1]]#, [["CSCI3155"], 1], [["CSCI3022"], 1], [["PHYS1140"], 1]]
+    main(choices)
     exit(0)
